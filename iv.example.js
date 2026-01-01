dce07eb23a9e3d840cb583ebf05121f45a4082ca
Taisuke Fukuno <fukuno@jig.jp>
2026-01-02 08:16:13 +0900

    rename

--- iv.example.js
+++ iv.example.js
@@ -1,9 +1,10 @@
 import { AESGCM } from "./AESGCM.js";
 import { sleep } from "https://js.sabae.cc/sleep.js";
+import { Base16 } from "https://code4fukui.github.io/Base16/Base16.js";
 
 const iv1 = AESGCM.createIV();
 for (;;) {
-  console.log(iv1);
+  console.log(Base16.encode(iv1));
   AESGCM.incrementIV(iv1);
   sleep(100);
 }
